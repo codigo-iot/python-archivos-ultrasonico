26c8966a92de710d4a8714e264cddeb01989a832
Hugo <hugoescalpelo@gmail.com>
2022-09-27 13:52:02 -0500

    Correcciones al programa de distancia

--- distancia.py
+++ distancia.py
@@ -18,23 +18,23 @@ f.write('TimeStamp,Value' + '\n')
 print ("Inicia la toma de datos")
 
 try:
-	while True:
-		print ("acerque el objeto para medir la distancia")
-		GPIO.output(GPIO_TRIGGER,True)
-		time.sleep(0.00001)
-		GPIO.output(GPIO_TRIGGER,False)
-		start = time.time()
-		while GPIO.input(GPIO_ECHO)==0:
-			start = time.time()
-		while GPIO.input(GPIO_ECHO)==1:
-			stop = time.time()
-		elapsed = stop-start
-		distance = (elapsed * 34300)/2
-		sTimeStamp = time.strftime('%Y%m%d%H%M%S')
-		f.write(sTimeStamp + ',' + str(distance) + '\n')
-		print (sTimeStamp) + (" ") + (str(distance))
-		time.sleep(1)
-		sTmpFileStamp = time.strftime('%Y%m%d%H')
+    while True:
+        print ("acerque el objeto para medir la distancia")
+        GPIO.output(GPIO_TRIGGER,True)
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER,False)
+        start = time.time()
+        while GPIO.input(GPIO_ECHO)==0:
+            start = time.time()
+        while GPIO.input(GPIO_ECHO)==1:
+            stop = time.time()
+        elapsed = stop-start
+        distance = (elapsed * 34300)/2
+        sTimeStamp = time.strftime('%Y%m%d%H%M%S')
+        f.write(sTimeStamp + ',' + str(distance) + '\n')
+        print (sTimeStamp + ' ' + str(distance))
+        time.sleep(1)
+        sTmpFileStamp = time.strftime('%Y%m%d%H')
         if sTmpFileStamp != sFileStamp:
             f.close
             sFileName = 'out/' + sTmpFileStamp + '.txt'
@@ -43,6 +43,6 @@ try:
             print ("creando el archivo")
 
 except KeyboardInterrupt:
-	print ('\n' + 'termina la captura de datos.' + '\n')
-	f.close
-	GPIO.cleanup()
+    print ('\n' + 'termina la captura de datos.' + '\n')
+    f.close
+    GPIO.cleanup()
